5d634cb05389f49890480c0e46baae076affda1f
roweit-matthewbarnett <matthew.barnett@roweit.co.uk>
2024-02-22 10:29:16 +0000

    [WDHV-970] Remove IBM references

--- atmospheric_order_download/CallProgram.bat
+++ atmospheric_order_download/CallProgram.bat
@@ -1,1 +1,1 @@
-python cda_download.py --runs latest --url https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/1.0.0 --secret YourSecretCode --client YourClientID --orders your_order
+python cda_download.py --runs latest --apikey YourApiKey --orders your_order
